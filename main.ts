9a081fd348114d09063cce7a9e1e13d927f1eca5
vlogger23 <100538071+vlogger23@users.noreply.github.com>
2025-03-04 11:19:08 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,19 @@
-input.onGesture(Gesture.TiltRight, function () {
-    basic.showIcon(IconNames.ArrowEast)
+input.onPinTouchEvent(TouchPin.P2, input.buttonEventValue(ButtonEvent.Hold), function () {
+    basic.showIcon(IconNames.Angry)
+    basic.setLedColor(0x00ffff)
+    music.playMelody("G F G A - F E D ", 120)
 })
-input.onGesture(Gesture.LogoUp, function () {
-    basic.showIcon(IconNames.ArrowSouth)
+input.onPinTouchEvent(TouchPin.P1, input.buttonEventValue(ButtonEvent.Hold), function () {
+    basic.showIcon(IconNames.Confused)
+    basic.setLedColor(0xff9da5)
+    music.playMelody("C5 A B G A F G E ", 120)
 })
-input.onGesture(Gesture.TiltLeft, function () {
-    basic.showIcon(IconNames.ArrowWest)
+input.onPinTouchEvent(TouchPin.P3, input.buttonEventValue(ButtonEvent.Hold), function () {
+    basic.showIcon(IconNames.Happy)
+    basic.setLedColors(0xff0000, 0xff0000, 0xff0000)
+    music.playMelody("C D E F G A B C5 ", 120)
 })
-input.onGesture(Gesture.LogoDown, function () {
-    basic.showIcon(IconNames.ArrowNorth)
+input.onPinTouchEvent(TouchPin.P0, input.buttonEventValue(ButtonEvent.Hold), function () {
+    basic.showIcon(IconNames.Scissors)
+    music.playMelody("C5 B A G F E D C ", 120)
 })
